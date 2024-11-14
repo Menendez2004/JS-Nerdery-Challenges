770f6a5f3fbfcbb41cee74a590d1564724ab2355
Kevin Menendez <kevinmenendezzepeda@gmail.com>
2024-11-14 10:20:54 -0600

    Im already finish the challenges All front end are already did it Also the jS-Algorithms are already done

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -14,6 +14,14 @@ Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 
 const readableTime = (seconds) => {
   // YOUR CODE HERE...
+  let hours = Math.floor(seconds / 3600).toString().padStart(2, '0'); //calculates the number of full hours in the given seconds.
+  let minutes = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0');// the same as above but divide the remaining seconds by 60 to get the minutes
+  let secs = Math.floor(seconds % 3600 % 60).toString().padStart(2, '0');//literally the same as above but obtain the module of the remaining seconds by 60 to get the seconds
+  // console.log(hours)//just for check iff the format is corrct xd
+  // console.log(minutes)
+  // console.log(secs)
+  return `${hours}:${minutes}:${secs}`;
+
 };
 
 readableTime(458);
@@ -42,11 +50,18 @@ const COUNTRY_NAMES = ["Germany", "Norway", "Island", "Japan", "Israel"];
 
 const circularArray = (index) => {
   // YOUR CODE HERE...
+  index = index % COUNTRY_NAMES.length; //adjust the the range of the index
+  let firstPart = COUNTRY_NAMES.slice(index); //get the first part of the array
+  let secondPart = COUNTRY_NAMES.slice(0, index); //get the second part of the array to pass at the beginning
+  // console.log(firstPart)
+  // console.log(secondPart)
+  return firstPart.concat(secondPart); //just concatenate both parts of the array 
 };
 
 circularArray(2);
 circularArray(3);
 circularArray(5);
+circularArray(9);
 circularArray(9);
 
 /* *****
@@ -71,7 +86,17 @@ The last 3 digits for the sum of powers from 1 to 10 is "317"
 
 const ownPower = (number, lastDigits) => {
   // YOUR CODE HERE...
+  let suma = BigInt(0); 
+  for(let i = 1; i <= number; i++){
+    suma += BigInt(i) ** BigInt(i); //calculate the power of the number and add it to the sum (ty for the page that u provided us to solve) 
+
+  }
+  // console.log(suma)
+  return suma.toString().slice(-lastDigits);
+
+
 };
+
 
 ownPower(10, 3);
 ownPower(12, 7);
@@ -96,6 +121,16 @@ Since 10! === 3628800 and you sum 3 + 6 + 2 + 8 + 8 + 0 + 0
 
 const digitSum = (n) => {
   // YOUR CODE HERE...
+   let factorial = BigInt(1);
+  for (let i = 1; i <= n; i++) {
+    factorial *= BigInt(i);
+  }
+  let fact = 0;
+  factorial.toString().split('').forEach(digit => fact += parseInt(digit));
+  // console.log(fact)
+  return fact;
+
+
 };
 
 digitSum(10);
@@ -119,6 +154,16 @@ Because the 12th index in the Fibonacci sequence is 144, and 144 has three digit
 
 const fibIndex = (n) => {
   // YOUR CODE HERE...
+  let fib = [BigInt(0), BigInt(1)];
+  let index = 2;
+  while (true) {
+    fib[index] = fib[index - 1] + fib[index - 2];
+    if (fib[index].toString().length === n) {
+      // console.log(fib)
+      return index
+    }
+    index++;
+  }
 };
 
 fibIndex(3);
